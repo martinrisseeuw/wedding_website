319cb33b979e5306b6fd4287f37a505c0f3ce4d3
martinrisseeuw <hello@martinr.nl>
2017-07-10 16:06:15 +0200

    nuxt config update removed eslint

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,20 +23,5 @@ module.exports = {
   /*
   ** Customize the progress-bar color
   */
-  loading: false,
-  build: {
-    /*
-    ** Run ESLINT on save
-    */
-    extend (config, ctx) {
-      if (ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
-  }
+  loading: false
 }
